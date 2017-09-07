af9bffe531ca2feef5d2a9f44cbd745dd8e316d3
KLiuCSUN <kevin.liu.149@my.csun.edu>
2017-09-07 15:47:44 -0400

    Initial project setup for Zuul

---
+++ zuul/src/main/java/com/revature/zuul/filters/ErrorFilter.java
@@ -0,0 +1,29 @@
+package com.revature.zuul.filters;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class ErrorFilter extends ZuulFilter{
+
+	@Override
+	public Object run() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		return "error";
+	}
+
+}

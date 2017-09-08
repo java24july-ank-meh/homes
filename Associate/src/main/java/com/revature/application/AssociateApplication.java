e5c75e23f2924056bc194a332b3f0204ab1226a8
Christian Roach <compcritter@gmail.com>
2017-09-08 11:45:00 -0400

    add some more pom stuff, edit application to be ready for kafka

--- Associate/src/main/java/com/revature/application/AssociateApplication.java
+++ Associate/src/main/java/com/revature/application/AssociateApplication.java
@@ -1,9 +1,13 @@
 package com.revature.application;
 
+import javax.xml.transform.Source;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class AssociateApplication {
 
 	public static void main(String[] args) {

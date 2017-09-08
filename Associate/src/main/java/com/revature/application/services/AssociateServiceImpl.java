d2432537327e36ae156113b99e53c453f940bd9d
Christian Roach <compcritter@gmail.com>
2017-09-08 14:49:45 -0400

    base kafka implementation

--- Associate/src/main/java/com/revature/application/services/AssociateServiceImpl.java
+++ Associate/src/main/java/com/revature/application/services/AssociateServiceImpl.java
@@ -2,8 +2,12 @@ package com.revature.application.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,8 +16,26 @@ import com.revature.application.repository.AssociateRepository;
 
 @Service
 public class AssociateServiceImpl implements AssociateService {
+
+	private final CountDownLatch latch = new CountDownLatch(3);
 	
 	private AssociateRepository associateRepository;
+	
+	 @Autowired
+	 private KafkaTemplate<String, String> template;
+	 /*example use
+	 this.template.send("myTopic", "foo1");
+     this.template.send("myTopic", "foo2");
+     this.template.send("myTopic", "foo3");
+     latch.await(60, TimeUnit.SECONDS);
+     System.out.println("Messages Sent");*/
+
+    @KafkaListener(topics = "myTopic")
+    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+        System.out.println("################# LISTENING TO MESSAGE ###################");
+        System.out.println(cr.toString());
+        latch.countDown();
+    }
 	
 	@Autowired
 	public void setAssociateRepository(AssociateRepository associateRepository) {

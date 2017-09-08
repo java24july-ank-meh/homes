9bfa78741b3d8020337289c8788c20cd312411c3
Amitai Ilan <amitai.ilan@gmail.com>
2017-09-08 15:32:00 -0400

    getters and setters for office

--- apartments/src/main/java/com/revature/application/model/Office.java
+++ apartments/src/main/java/com/revature/application/model/Office.java
@@ -27,4 +27,72 @@ public class Office {
 	
 	public Office() {
 	}
+
+	public Office(int officeId, String street, String city, String state, String zip, String phone, String website) {
+		super();
+		this.officeId = officeId;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phone = phone;
+		this.website = website;
+	}
+
+	public int getOfficeId() {
+		return officeId;
+	}
+
+	public void setOfficeId(int officeId) {
+		this.officeId = officeId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	
 }

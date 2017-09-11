d71b8731ff226ec727d4c47264fd362fe0096e17
Huriel Hernandez <huriel.hernandez01@utrgv.edu>
2017-09-11 15:15:22 -0400

    Add ( Kafka ): Add Kafka on pom and add snippet on service

--- RequestService/src/main/java/com/revature/application/services/MaintenanceService.java
+++ RequestService/src/main/java/com/revature/application/services/MaintenanceService.java
@@ -1,8 +1,11 @@
 package com.revature.application.services;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +20,9 @@ public class MaintenanceService
 {
 	@Autowired
 	MaintenanceRepository maintenanceRepository;
+	
+	 @Autowired
+	 KafkaTemplate<String, String> template;
 	
 	public List<Maintenance> findAll(){
 		return maintenanceRepository.findAll();
@@ -36,5 +42,24 @@ public class MaintenanceService
 	
 	public int update(Maintenance maintenance){
 		return maintenanceRepository.save(maintenance).getMaintenanceId();
+	}
+	
+
+	public void run(String... args) throws Exception
+	{
+		// this.template.send("myTopic", "foo1");
+		// this.template.send("myTopic", "foo2");
+		// this.template.send("myTopic", "foo3");
+		// latch.await(60, TimeUnit.SECONDS);
+		// System.out.println("Messages Sent");
+	}
+
+	@KafkaListener(topics = "request")
+	public void listen(ConsumerRecord<?, ?> cr) throws Exception
+	{
+		// System.out.println("################# LISTENING TO MESSAGE
+		// ###################");
+		// System.out.println(cr.toString());
+		// latch.countDown();
 	}
 }

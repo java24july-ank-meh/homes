8c136a11f96519b6ba979f169c444ca9ff5d8966
cherboth <calebherboth@gmail.com>
2017-09-12 13:13:13 -0400

    fixed merge probs

--- apartments/src/main/java/com/revature/application/controller/OfficeController.java
+++ apartments/src/main/java/com/revature/application/controller/OfficeController.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package com.revature.application.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.revature.application.model.Office;
-import com.revature.application.service.OfficeService;
-
-@RestController
-@RequestMapping("/office")
-public class OfficeController {
-	@Autowired
-	OfficeService os;
-	
-	@GetMapping
-	public List<Office> findAll() {
-		return os.findAll();
-	}
-	
-	@GetMapping("{id}")
-	public Office findOne(@PathVariable("id") int id) {
-		return os.findOne(id);
-	}
-}
-=======
 package com.revature.application.controller;
 
 import java.util.List;
@@ -61,4 +30,3 @@ public class OfficeController {
 		return os.save(new Office(0,"street","city","state","zip","phone","website"));
 	}
 }
->>>>>>> f4f780e488622afc022d338905120ecf6c28909c

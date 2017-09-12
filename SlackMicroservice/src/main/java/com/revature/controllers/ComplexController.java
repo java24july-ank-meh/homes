7f18ace885fc207d5b790d76728564b3d6415dc7
Chris Palmour <chpalmour@gmail>
2017-09-12 13:12:39 -0400

    Add complex controller

--- SlackMicroservice/src/main/java/com/revature/controllers/ComplexController.java
+++ SlackMicroservice/src/main/java/com/revature/controllers/ComplexController.java
@@ -1,17 +1,5 @@
 package com.revature.controllers;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -29,12 +17,12 @@ import org.springframework.web.client.RestTemplate;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wordnik.swagger.annotations.Api;
 
-@Api(value="unit", description="This service creates, updates, and deletes slack channels for individual units")
+@Api(value="unit", description="This service creates, updates, and deletes slack channels for apartment complexes")
 @Service
 @RestController
-@RequestMapping("unit")
-public class UnitController {
-	
+@RequestMapping("complex")
+public class ComplexController {
+
 	/*restTemplate bean handles all http requests to slack API. A single RestTemplate
 	 * object can be used for multiple requests.
 	 */
@@ -44,13 +32,12 @@ public class UnitController {
 	Helper helper;
 	
 	@HystrixCommand(fallbackMethod="createFallback")
-	@PostMapping("create/{complex}/{unit}")
-	public ResponseEntity<String> createUnit(@PathVariable("complex") String complex, 
-			@PathVariable("unit") String unit) {
+	@PostMapping("create/{complex}")
+	public ResponseEntity<String> createUnit(@PathVariable("complex") String complex) {
 		
 		/*The url string includes the endpoint and all necessary parameters. For slack's 
 		 *channel.create method, we need the app token and complex name.*/
-		String channelName = complex + unit;
+		
 		String url = null;
 		String channelId = null;
 		
@@ -61,13 +48,13 @@ public class UnitController {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		
-		if(helper.channelNameIsUnique(channelName)) {
+		if(helper.channelNameIsUnique(complex)) {
 			//needs token and channelName 
 			url = "https://slack.com/api/channels.create";
-			params.add("name", channelName);
+			params.add("name", complex);
 		}
 		else {
-			channelId = helper.getChannelId(channelName);
+			channelId = helper.getChannelId(complex);
 			//needs token and channelId
 			url = "https://slack.com/api/channels.unarchive";
 			params.add("channel", channelId);
@@ -78,19 +65,15 @@ public class UnitController {
 	 
 		ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 	
-		channelId = helper.getChannelId(channelName);
+		channelId = helper.getChannelId(complex);
 		System.out.println("new channel id: " + channelId);
 		
 		return response;
 	}
 	
-	@PostMapping("update/{complex}/{unit}/{new-complex}/{new-unit}")
+	@PostMapping("update/{complex}/{new-complex}")
 	public ResponseEntity<String> updateUnit(@PathVariable("complex") String complex, 
-			@PathVariable("unit") String unit, @PathVariable("new-complex") String newComplex,
-			@PathVariable("new-unit") String newUnit) {
-		
-		String channelName = complex + unit;
-		String newName = newComplex + newUnit;
+			@PathVariable("new-complex") String newComplex) {
 		
 		//needs token, channel, name
 		String url = "https://slack.com/api/channels.rename";
@@ -98,8 +81,8 @@ public class UnitController {
 		MultiValueMap<String, String> params = 
 				new LinkedMultiValueMap<String, String>();
 		params.add("token", Helper.slackProps.get("client_token"));
-		params.add("channel", helper.getChannelId(channelName));
-		params.add("name", newName);
+		params.add("channel", helper.getChannelId(complex));
+		params.add("name", newComplex);
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -112,12 +95,10 @@ public class UnitController {
 		return response;
 	}
 	
-	@PostMapping("delete/{complex}/{unit}")
-	public ResponseEntity<String> deleteUnit(@PathVariable("complex") String complex, 
-			@PathVariable("unit") String unit) {
+	@PostMapping("delete/{complex}")
+	public ResponseEntity<String> deleteUnit(@PathVariable("complex") String complex) {
 		
-		String channelName = complex + unit;
-		String channelId = helper.getChannelId(channelName);
+		String channelId = helper.getChannelId(complex);
 		
 		//needs token, channel
 		String url = "https://slack.com/api/channels.archive";
@@ -141,4 +122,5 @@ public class UnitController {
 	public void createFallback() {
 		System.out.println("Reached create fallback");
 	}
+	
 }

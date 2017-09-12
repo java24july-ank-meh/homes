bb362269df41a1202ebefe427c80214cc7539024
jackson.demaree <jackson.demaree@tamu.edu>
2017-09-12 13:46:40 -0400

    ResidentController invite

--- SlackMicroservice/src/main/java/com/revature/controllers/ResidentController.java
+++ SlackMicroservice/src/main/java/com/revature/controllers/ResidentController.java
@@ -2,6 +2,7 @@ package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ public class ResidentController {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Autowired
+	Helper helper;
+	
+	/*Sends an email invite to the email and autofills first name and last name for slack registration
+	does not enforce names*/
 	@PostMapping("invite")
 	public  ResponseEntity<Object> invite(String token, String email, String firstName, String lastName){
 	String requestUrl = "https://slack.com/api/users.admin.invite?token=" +
@@ -25,5 +31,22 @@ public class ResidentController {
 	return ResponseEntity.ok(responseString);
 	}
 
+	@PostMapping("message/{complex}/{unit}")
+	public ResponseEntity<Object> messageChannel(String token, String channelName, String message){
+		
+		String channelId = helper.getChannelId(channelName);
+		
+		String requestUrl  = "https://slack.com/api/chat.postMessage?token=" + token +
+				 "&channel="+ channelId+"&text=" + message;
+		String responseString = restTemplate.getForObject(requestUrl, String.class);
+		return ResponseEntity.ok(responseString);
+		
+	}
+	
+	@PostMapping("message/user")
+	public ResponseEntity<Object> messageUser(){
+		return null;
+		
+	}
 	
 }

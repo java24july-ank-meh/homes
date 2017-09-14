20c80328d690da4fda561c5053e44317d8ff6569
Madrone Goldman <madronegoldman@gmail.com>
2017-09-14 13:37:59 -0400

    resident controller is now perfect

--- SlackMicroservice/src/main/java/com/revature/controllers/Helper.java
+++ SlackMicroservice/src/main/java/com/revature/controllers/Helper.java
@@ -3,8 +3,10 @@ package com.revature.controllers;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -36,7 +38,11 @@ public class Helper {
 		Properties props = new Properties();
 		
 		try {
+			System.out.println(" \n\n\n\n RUNNING STATIC BLOCK \n\n\n\n");
 			file = new FileInputStream("src/main/java/slack.properties");
+			// \/\/ shitty madrone version \/\/
+			//file = new FileInputStream("C:\\docs\\Revature\\batch-src\\homes\\SlackMicroservice\\src\\main\\java\\slack.properties");
+			// /\/\ shitty madrone version /\/\
 			props.load(file);
 			Set<String> propNames = props.stringPropertyNames();
 			for(String name : propNames) {
@@ -102,10 +108,124 @@ public class Helper {
 		
 	}
 	
-	public String referenceAllUsersInMessage() {
-		String url = "https://slack.com/api/channels.list&token=" + slackProps.get("client_token");
-		String responseString = restTemplate.getForObject(url, String.class);
-		return url;
+	/*	Returns ArrayList with the slack ID's of all users in a given channel*/
+	
+	public List<String> getAllUsersInChannel(String channelName) {
+		
+		String channelId = getChannelId(channelName);
+		
+		
+		String requestUrl = "https://slack.com/api/channels.info?token=" +slackProps.get("client_token") +
+				 "&channel="+ channelId;
+		
+		String responseString = restTemplate.getForObject(requestUrl, String.class);
+		JsonNode rootNode, channelNode, memNode = null;
+		List<String> users = new ArrayList<String>();
+		
+		try {
+			rootNode = objectMapper.readTree(responseString);
+			channelNode = rootNode.path("channel");
+			memNode = channelNode.path("members");
+			
+			Iterator<JsonNode> elements = memNode.elements();
+			while(elements.hasNext()){
+				JsonNode member = elements.next();
+				users.add(member.asText());
+				users.add(getUserName(member.asText()) );
+			}
+			System.out.println(users);
+			return users;
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/*	Returns string with UserName given slack users ID*/
+	public String getUserName(String userId) {
+		
+		String requestUrl = "https://slack.com/api/users.info?token=" +slackProps.get("client_token") +
+				 "&user="+ userId;
+		
+		String responseString = restTemplate.getForObject(requestUrl, String.class);
+		JsonNode rootNode, userNode, idNode = null;
+		String userName = "";
+		
+		try {
+			rootNode = objectMapper.readTree(responseString);
+			userNode = rootNode.path("user");
+			idNode = userNode.path("name");
+			userName = idNode.asText();
+			
+			return userName;
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public String  startConvo(List<String> userIds, String channelId) {
+		
+		String tag = "channel";
+		String requestUrl = "https://slack.com/api/conversations.open?token=" +slackProps.get("client_token") +
+				 "&return_im=false" + "&users=";
+		if(userIds.size()>1) {
+			requestUrl = "https://slack.com/api/mpim.open?token=" +slackProps.get("client_token") +"&users=";
+			tag = "group";
+		}
+		
+		for(int i = 0; i<(userIds.size()-1); i++) {
+			requestUrl += userIds.get(i) + ",";
+		}
+		requestUrl += userIds.get(userIds.size()-1);
+		requestUrl +="&pretty=1";
+		
+		String responseString = restTemplate.getForObject(requestUrl, String.class);
+		JsonNode rootNode, chanNode, idNode = null;
+		String channel = "";
+		
+		try {
+			rootNode = objectMapper.readTree(responseString);
+			chanNode = rootNode.path(tag);
+			idNode = chanNode.path("id");
+			channel = idNode.asText();
+			
+
+			return channel;
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public String  directMessage(String token, String channelId, String message, List<String> userIds) {
+		
+		String newChannel = startConvo(userIds, channelId);
+		
+		String requestUrl = "https://slack.com/api/chat.postMessage?token=" +slackProps.get("client_token") +
+				"&channel="+newChannel + "&return_im=false" + "&text="+message;
+			
+		String responseString = restTemplate.getForObject(requestUrl, String.class);
+		return responseString;
+	}
+	
+	public String  multiMessage(String token, String channelId, String message, List<String> userIds) {
+		
+		List<String> channels = new ArrayList<String>();
+		String responseString = "";
+		
+		for(int i = 0; i < userIds.size(); i++) {
+			channels.add(startConvo(userIds, channelId) );
+			List<String> singleUser = new ArrayList<String>();
+			singleUser.add(userIds.get(i));
+			
+			responseString = directMessage(token, channelId, message, singleUser);
+		}
+				
+		return responseString;
 	}
 	
 }
--- SlackMicroservice/src/main/java/com/revature/controllers/ResidentController.java
+++ SlackMicroservice/src/main/java/com/revature/controllers/ResidentController.java
@@ -1,13 +1,24 @@
 package com.revature.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import com.wordnik.swagger.annotations.Api;
 
+@Api(value="unit", description="This service allows users to message other users or their channel over slack")
 @RestController
 @RequestMapping("resident")
 public class ResidentController {
@@ -16,12 +27,19 @@ public class ResidentController {
 	RestTemplate restTemplate;
 	
 	@Autowired
-	Helper helper;
+	Helper helper ;
+	
 	
 	/*Sends an email invite to the email and autofills first name and last name for slack registration
 	does not enforce names*/
 	@PostMapping("invite")
-	public  ResponseEntity<Object> invite(String token, String email, String firstName, String lastName){
+	public  ResponseEntity<Object> invite(HttpServletRequest req){
+	
+	String email = req.getParameter("email"); 
+	String firstName = req.getParameter("fname");
+	String lastName = req.getParameter("lname");
+	
+	String token = Helper.slackProps.get("client_token");
 	String requestUrl = "https://slack.com/api/users.admin.invite?token=" +
 			token +"&email=" + email +
 	"&first_name=" + firstName + "&last_name=" + lastName;
@@ -32,8 +50,11 @@ public class ResidentController {
 	}
 
 	@PostMapping("message/{complex}/{unit}")
-	public ResponseEntity<Object> messageChannel(String token, String channelName, String message){
+	public ResponseEntity<Object> messageChannel(@PathVariable("complex") String complex, @PathVariable("unit") String unit, 
+			@RequestPart("message") String message){
 		
+		String token = Helper.slackProps.get("client_token");
+		String channelName = complex + unit;
 		String channelId = helper.getChannelId(channelName);
 		
 		String requestUrl  = "https://slack.com/api/chat.postMessage?token=" + token +
@@ -43,9 +64,85 @@ public class ResidentController {
 		
 	}
 	
-	@PostMapping("message/user")
-	public ResponseEntity<Object> messageUser(){
-		return null;
+	/*sends a message and @mentions all users in the userIds List*/
+	@PostMapping("message/users/{complex}/{unit}")
+	public ResponseEntity<Object> messageUser(@PathVariable("complex") String complex, @PathVariable("unit") String unit, 
+			HttpServletRequest req){
+		
+		String message = req.getParameter("message");
+		String ids = req.getParameter("ids");
+		
+		List<String>userIds = new ArrayList<String>(Arrays.asList(ids.split(",")));
+		String token = Helper.slackProps.get("client_token");
+		String channelName = complex + unit;
+		String channelId = helper.getChannelId(channelName);
+		
+		String requestUrl  = "https://slack.com/api/chat.postMessage?token=" + token +
+				 "&channel="+ channelId+"&text=" + message;
+		for(int i = 0; i<userIds.size(); i++) {
+			requestUrl += " <@" + userIds.get(i) + ">";
+		}
+		
+		String responseString = restTemplate.getForObject(requestUrl, String.class);
+		return ResponseEntity.ok(responseString);
+		
+	}
+	
+	@PostMapping("message/users/direct/{complex}/{unit}")
+	public ResponseEntity<Object> messageUserDirect(@PathVariable("complex") String complex, @PathVariable("unit") String unit, 
+			HttpServletRequest req){
+		
+		String message = req.getParameter("message");
+		String group = req.getParameter("group");
+		String ids = req.getParameter("ids");
+		
+		List<String>userIds = new ArrayList<String>(Arrays.asList(ids.split(",")));
+		System.out.println(userIds);
+		
+		String token = Helper.slackProps.get("client_token");
+		String channelName = complex + unit;
+		String channelId = helper.getChannelId(channelName);
+		String responseString ="";
+		
+		//individual message/s
+		if(group.equals("0")) {
+			responseString = helper.multiMessage(token, channelId, message, userIds);
+		}
+		//group direct message
+		else {
+			responseString = helper.directMessage(token, channelId, message, userIds);
+		}
+
+		return ResponseEntity.ok(responseString);
+		
+	}
+	
+	/*kicks a user from an apartment channel*/
+	@PostMapping("kick/users/{complex}/{unit}")
+	public ResponseEntity<Object> kickUser(@PathVariable("complex") String complex, 
+			@PathVariable("unit") String unit, @RequestBody String userId){
+		
+		String token = Helper.slackProps.get("client_token");
+		String channelName = complex + unit;
+		String channelId = helper.getChannelId(channelName);
+		
+		String requestUrl  = "https://slack.com/api/channels.kick?token=" + token +
+				 "&channel="+ channelId+"&user=" + userId;
+		
+		String responseString = restTemplate.getForObject(requestUrl, String.class);
+		return ResponseEntity.ok(responseString);
+		
+	}
+	
+	/*send list of [userid1,username1, uid2,un2,...] to front end*/
+	@RequestMapping(value="message/listUsers/{complex}/{unit}", method=RequestMethod.GET)
+	public ResponseEntity<Object> listUser(@PathVariable("complex") String complex, @PathVariable("unit") String unit){
+		
+		String channelName = complex + unit;
+		List<String> userList = helper.getAllUsersInChannel(channelName);
+		//System.out.println(userList);
+		
+		return ResponseEntity.ok(userList);
 		
 	}
 	

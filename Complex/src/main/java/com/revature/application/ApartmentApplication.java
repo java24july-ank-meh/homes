5b614832944e342155cfe44310317c33778b9a20
Amitai Ilan <amitai.ilan@gmail.com>
2017-09-18 10:54:35 -0400

    fixing eureka

--- Complex/src/main/java/com/revature/application/ApartmentApplication.java
+++ Complex/src/main/java/com/revature/application/ApartmentApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @EnableEurekaClient
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ApartmentApplication {
 	

9c34bd80b34b47ac568f5879ba39de1e9be52764
Christian Roach <compcritter@gmail.com>
2017-09-19 16:58:47 -0400

    notification implemented, complexcomposite can create notifications

--- RequestService/src/main/java/com/revature/application/repository/NotificationRepository.java
+++ RequestService/src/main/java/com/revature/application/repository/NotificationRepository.java
@@ -1,9 +1,11 @@
 package com.revature.application.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.application.models.Notification;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
 	
 }

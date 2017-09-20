a1f1c6d0f44e8cd9aa484b623c6ed28ef7876e94
Christian Roach <compcritter@gmail.com>
2017-09-20 15:52:40 -0400

    REquestComposite messaging in place
    waitinng on the slack direct message to be pushed in order to further
    this.

--- RequestComposite/src/main/java/com/revature/application/controllers/RequestCompositeController.java
+++ RequestComposite/src/main/java/com/revature/application/controllers/RequestCompositeController.java
@@ -1,8 +1,27 @@
 package com.revature.application.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class RequestCompositeController {
+import com.revature.application.services.RequestCompositeService;
 
+@RestController
+@RequestMapping("requestcomposite")
+public class RequestCompositeController {
+	
+	@Autowired
+	RequestCompositeService requestService;
+	
+	@PostMapping("{id}")
+	public ResponseEntity<Object> sendMessage(@PathVariable("id") int id, @RequestBody String message) {
+		
+		requestService.sendMessage(id, message);
+		
+		return ResponseEntity.ok().build();
+	}
 }
---
+++ RequestComposite/src/main/java/com/revature/application/models/Associate.java
@@ -0,0 +1,205 @@
+package com.revature.application.models;
+
+import java.time.LocalDateTime;
+
+public class Associate {
+	private Long associateId;
+	private String firstName;
+	private String lastName;
+	private String slackId;
+	private String role;
+	private Long officeId;
+	private String phone;
+	private String about;
+	private String email;
+	private String gender;
+	private Long unitId;
+	private int hasCar = 0; // fake boolean
+	private LocalDateTime moveInDate;
+	private LocalDateTime moveOutDate;
+	private LocalDateTime housingAgreed;// if not null then its agreed
+	private LocalDateTime hasKeys; // if null currently does not have keys
+	private String username;
+	private String password;
+
+	public Associate() {
+		super();
+	}
+
+	public Associate(Long associateId, String firstName, String lastName, String slackId, String role, Long officeId,
+			String phone, String about, String email, String gender, Long unitId, int hasCar, LocalDateTime moveInDate,
+			LocalDateTime moveOutDate, LocalDateTime housingAgreed, LocalDateTime hasKeys, String username,
+			String password) {
+		super();
+		this.associateId = associateId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.slackId = slackId;
+		this.role = role;
+		this.officeId = officeId;
+		this.phone = phone;
+		this.about = about;
+		this.email = email;
+		this.gender = gender;
+		this.unitId = unitId;
+		this.hasCar = hasCar;
+		this.moveInDate = moveInDate;
+		this.moveOutDate = moveOutDate;
+		this.housingAgreed = housingAgreed;
+		this.hasKeys = hasKeys;
+	}
+
+	public Long getAssociateId() {
+		return associateId;
+	}
+
+	public void setAssociateId(Long associateId) {
+		this.associateId = associateId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSlackId() {
+		return slackId;
+	}
+
+	public void setSlackId(String slackId) {
+		this.slackId = slackId;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Long getOfficeId() {
+		return officeId;
+	}
+
+	public void setOfficeId(Long officeId) {
+		this.officeId = officeId;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Long getUnitId() {
+		return unitId;
+	}
+
+	public void setUnitId(Long unitId) {
+		this.unitId = unitId;
+	}
+
+	public int getHasCar() {
+		return hasCar;
+	}
+
+	public void setHasCar(int hasCar) {
+		this.hasCar = hasCar;
+	}
+
+	public LocalDateTime getMoveInDate() {
+		return moveInDate;
+	}
+
+	public void setMoveInDate(LocalDateTime moveInDate) {
+		this.moveInDate = moveInDate;
+	}
+
+	public LocalDateTime getMoveOutDate() {
+		return moveOutDate;
+	}
+
+	public void setMoveOutDate(LocalDateTime moveOutDate) {
+		this.moveOutDate = moveOutDate;
+	}
+
+	public LocalDateTime getHousingAgreed() {
+		return housingAgreed;
+	}
+
+	public void setHousingAgreed(LocalDateTime housingAgreed) {
+		this.housingAgreed = housingAgreed;
+	}
+
+	public LocalDateTime getHasKeys() {
+		return hasKeys;
+	}
+
+	public void setHasKeys(LocalDateTime hasKeys) {
+		this.hasKeys = hasKeys;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Associate [associateId=" + associateId + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", slackId=" + slackId + ", role=" + role + ", officeId=" + officeId + ", phone=" + phone + ", about="
+				+ about + ", email=" + email + ", gender=" + gender + ", unitId=" + unitId + ", hasCar=" + hasCar
+				+ ", moveInDate=" + moveInDate + ", moveOutDate=" + moveOutDate + ", housingAgreed=" + housingAgreed
+				+ ", hasKeys=" + hasKeys + ", username=" + username + ", password=" + password + "]";
+	}
+
+}

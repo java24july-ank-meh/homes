8461605ccb840dc432c8a6e54841aa4063204f77
KelvinL95 <Kelvin.lane.95@live.com>
2017-09-21 10:48:12 -0400

    work in progress

--- Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
+++ Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +25,9 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.revature.application.model.Complex;
 import com.revature.application.model.Office;
@@ -31,26 +35,42 @@ import com.revature.application.service.ComplexService;
 
 public class ComplexControllerTest {
 
-	@Mock
-	ComplexService cs;
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+    
+    private MockMvc mockMvc;
 	
+	@Mock
+	ComplexService mockCS;
+	@InjectMocks
+    private ComplexController complexController;
+	
+	List<Complex> complexes;
 	
 	@Before
 	public void setUp() throws Exception {
 		
+		MockitoAnnotations.initMocks(this);
+				
+		this.mockMvc = MockMvcBuilders.standaloneSetup(complexController).build();
+
 		Office o1 = new Office("address1", "phone1", "website1", "timezone1");
 		Office o2 = new Office("address2", "phone2", "website2", "timezone2");
 		
-		
-		List<Complex> complexes = new ArrayList<Complex>();
+		complexes = new ArrayList<Complex>();
 		Complex c1 = new Complex(o1.getWebsite(), "email1", o1.getPhone(), "name1", "abbr1", o1.getAddress(), "parking1", "photoUrl1", o1);
 		Complex c2= new Complex(o2.getWebsite(), "email2", o2.getPhone(), "name2", "abbr2", o2.getAddress(), "parking2", "photoUrl2", o2);
 		
+		complexes.add(c1);
+		complexes.add(c2);		
 		
 	}
 
 	@Test
 	public void testFindAll() {
+		when(mockCS.findAll()).thenReturn(complexes);
+		
+		
 		fail("Not yet implemented");
 	}
 

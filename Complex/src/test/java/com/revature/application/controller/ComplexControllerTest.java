e5ea5f7cf9ee439e5f0a07ac8b4eb3fc5ea78ab4
KelvinL95 <Kelvin.lane.95@live.com>
2017-09-21 10:42:43 -0400

    work in progress

--- Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
+++ Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
@@ -14,6 +14,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,6 +25,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.revature.application.model.Complex;
+import com.revature.application.model.Office;
 import com.revature.application.service.ComplexService;
 
 public class ComplexControllerTest {
@@ -32,8 +38,13 @@ public class ComplexControllerTest {
 	@Before
 	public void setUp() throws Exception {
 		
+		Office o1 = new Office("address1", "phone1", "website1", "timezone1");
+		Office o2 = new Office("address2", "phone2", "website2", "timezone2");
 		
 		
+		List<Complex> complexes = new ArrayList<Complex>();
+		Complex c1 = new Complex(o1.getWebsite(), "email1", o1.getPhone(), "name1", "abbr1", o1.getAddress(), "parking1", "photoUrl1", o1);
+		Complex c2= new Complex(o2.getWebsite(), "email2", o2.getPhone(), "name2", "abbr2", o2.getAddress(), "parking2", "photoUrl2", o2);
 		
 		
 	}

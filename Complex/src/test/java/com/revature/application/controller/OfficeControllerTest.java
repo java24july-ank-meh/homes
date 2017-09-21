1c5de3cc09516a2f94ce9a49b39643b146076eba
Amitai Ilan <amitai.ilan@gmail.com>
2017-09-21 10:23:01 -0400

    added office controller test

---
+++ Complex/src/test/java/com/revature/application/controller/OfficeControllerTest.java
@@ -0,0 +1,39 @@
+package com.revature.application.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OfficeControllerTest {
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindComplexes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}

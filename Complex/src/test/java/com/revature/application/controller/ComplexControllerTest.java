a8742f99975f0a2d3277916c9402959e26ea93ab
KelvinL95 <Kelvin.lane.95@live.com>
2017-09-21 10:16:05 -0400

    added CompanyControllerTest

--- Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
+++ Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
@@ -1,5 +1,44 @@
 package com.revature.application.controller;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ComplexControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindUnits() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateComplex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateComplex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteComplex() {
+		fail("Not yet implemented");
+	}
 
 }

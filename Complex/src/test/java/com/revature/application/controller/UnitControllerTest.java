bf3c1886ed3cd1a0da87cbc764bb05c4dca50f07
Amitai Ilan <amitai.ilan@gmail.com>
2017-09-21 10:17:44 -0400

    created unit controller test

---
+++ Complex/src/test/java/com/revature/application/controller/UnitControllerTest.java
@@ -0,0 +1,34 @@
+package com.revature.application.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitControllerTest {
+
+	@Test
+	public void testDisplayAllUnit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayUnit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateUnit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUnit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteUnit() {
+		fail("Not yet implemented");
+	}
+
+}

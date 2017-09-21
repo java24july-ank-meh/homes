52704b1aa15d15dbe0b3db36138be958d2715bb9
KelvinL95 <Kelvin.lane.95@live.com>
2017-09-21 10:25:49 -0400

    added spring boot test starter

--- Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
+++ Complex/src/test/java/com/revature/application/controller/ComplexControllerTest.java
@@ -2,13 +2,40 @@ package com.revature.application.controller;
 
 import static org.junit.Assert.*;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.revature.application.service.ComplexService;
+
 public class ComplexControllerTest {
 
+	@Mock
+	ComplexService cs;
+	
+	
 	@Before
 	public void setUp() throws Exception {
+		
+		
+		
+		
+		
 	}
 
 	@Test

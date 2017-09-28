cee14cc33e09d9275748e792611353ff69852217
Huriel Hernandez <huriel.hernandez01@utrgv.edu>
2017-09-28 00:20:17 -0400

    Bugfix(Sidenav): Call api call if rootScope.rootUser does not contain isManager

--- client/src/main/resources/static/SideNav/sidenavController.js
+++ client/src/main/resources/static/SideNav/sidenavController.js
@@ -89,7 +89,7 @@ angular.module('rhmsApp').controller('sidenavController', ['$scope', '$mdBottomS
 
    if(!$rootScope.rootUser)
     	$state.go("logout");
-   else
+   else if($rootScope.rootUser.isManager == undefined)
     $http.post("/api/slack/resident/admin",{email : $rootScope.rootUser.email, token:$rootScope.rootUser.token}).then(function(response){
     	$rootScope.rootUser.isManager = response.data;
     	$scope.isManager = $rootScope.rootUser.isManager ? "Manager" : "Resident";
